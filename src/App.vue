6732d4c63c15f7e72edc0e11335305d2da11635f
katherineus <jekaterina.neustroeva@gmail.com>
2024-03-05 00:34:00 +0200

    Add new files and configurations for Tailwind CSS and Vite

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <main class="container mx-auto">
+    <h1>Test</h1>
+    <HelloWorld />
+  </main>
+</template>
+
+<style scoped>
+</style>
